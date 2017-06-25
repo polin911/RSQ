4cc3de6f04211ca5230159e2a2671a2e1515aeaa
polin911 <polin911@mail.ru>
2017-06-25 11:01:31 +0300

    Final vc works

--- RybaSushiQuize/FinalVC.swift
+++ RybaSushiQuize/FinalVC.swift
@@ -14,12 +14,15 @@ class FinalVC: UIViewController {
     var player = Player()
     
     @IBOutlet var lblTxt: UILabel!
+    var score = 0
+    var textForScore = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        lblTxt.text = String(player.playerScore)
+//        lblTxt.text = String(player.playerScore)
 
+        updateScore()
         // Do any additional setup after loading the view.
     }
 
@@ -30,6 +33,54 @@ class FinalVC: UIViewController {
         
     }
     
+    func updateScore() {
+        score = player.playerScore
+        if player.nameGame == player.wchiGame[0] {
+            switch score {
+            case 0:
+              lblTxt.text = "ты растроил Семена"
+                img.image = #imageLiteral(resourceName: "unSmile")
+            case 1...2:
+                lblTxt.text = "Семен добрый он тебя угостит кусочком авакадо "
+                img.image = #imageLiteral(resourceName: "smile")
+            case 3:
+                lblTxt.text = "Ты молодец, ты заработал \(score) очков! Получай свой обед! "
+                img.image = #imageLiteral(resourceName: "glass")
+            default:
+                lblTxt.text = "стоит переиграть"
+            }
+        }
+        if player.nameGame == player.wchiGame[1] {
+            switch score {
+            case 0:
+                lblTxt.text = "ты растроил Семена"
+                img.image = #imageLiteral(resourceName: "unSmile")
+            case 1...3:
+                lblTxt.text = "ты заработал \(score) балла Семен добрый он тебя угостит кусочком авакадо "
+                img.image = #imageLiteral(resourceName: "smile")
+            case 5:
+                lblTxt.text = "Ты молодец, ты заработал \(score) очков! Получай свой обед! "
+                img.image = #imageLiteral(resourceName: "glass")
+            default:
+                lblTxt.text = "стоит переиграть"
+            }
+        }
+        if player.nameGame == player.wchiGame[2] {
+            switch score {
+            case 0:
+                lblTxt.text = "ты растроил Семена"
+                img.image = #imageLiteral(resourceName: "unSmile")
+            case 1...3:
+                lblTxt.text = "ты заработал \(score)Семен добрый он тебя угостит тремя устрицами "
+                img.image = #imageLiteral(resourceName: "smile")
+            case 5...7:
+                lblTxt.text = "Ты молодец, ты заработал \(score) очков! Рома тебе сейчас откроет 12 устриц "
+                img.image = #imageLiteral(resourceName: "glass")
+            default:
+                lblTxt.text = "стоит переиграть"
+            }
+        }
+    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
       
         self.player.playerScore = 0
