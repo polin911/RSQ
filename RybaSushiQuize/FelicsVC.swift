840ac41c2d74899687cbb27ec16fd86da5554d8e
polin911 <polin911@mail.ru>
2017-06-27 17:57:06 +0300

    firs

--- RybaSushiQuize/FelicsVC.swift
+++ RybaSushiQuize/FelicsVC.swift
@@ -13,13 +13,13 @@ class FelicsVC: UIViewController {
     
     var player = Player()
     @IBOutlet var lblTxt: UILabel!
-    
+    var score = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        lblTxt.text = "\(player.playerName) ты заработал  \(player.playerScore) забирай свои \(player.nameGame) и ...."
+        updateScore()
         // Do any additional setup after loading the view.
     }
 
@@ -30,6 +30,20 @@ class FelicsVC: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let vc = segue.destination as! WhichQuizeVC
         vc.player.playerName = self.player.playerName
+    }
+    
+    func updateScore() {
+        score = player.playerScore
+        switch score {
+        case 0:
+            lblTxt.text = "Феликс подумал что ты тупой"
+        case 1...3:
+            lblTxt.text = "ты угадал всего \(score) Феликс разачарован"
+        case 3...7:
+            lblTxt.text = "\(score)Феликс гордиться тобой"
+        default:
+            lblTxt.text = "ты бот ??"
+        }
     }
 
 }
--- RybaSushiQuize/MainVC.swift
+++ RybaSushiQuize/MainVC.swift
@@ -7,29 +7,62 @@
 //
 
 import UIKit
+import Spring
+import RazzleDazzle
 
-class MainVC: UIViewController {
+class MainVC: AnimatedPagingScrollViewController {
 
+    @IBOutlet var sprView: SpringImageView!
+    
+    @IBOutlet var sprTxtLbl: SpringLabel!
+    
+    @IBOutlet var txtFirstImg: UIImageView!
+    
+    @IBOutlet var btnEnter: UIButton!
+    
+    @IBOutlet var pageCont: UIPageControl!
+    
+    
+    //@IBOutlet var mainView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        sprAnimation()
+        razDaz()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        pageCont.currentPage = Int(pageOffset + 0.5)
+    }
+    override func numberOfPages() -> Int {
+        return 4
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func razDaz() {
+        //contentView.addSubview(mainView)
+        contentView.addSubview(sprView)
+        contentView.addSubview(sprTxtLbl)
+        contentView.addSubview(txtFirstImg)
+        contentView.addSubview(btnEnter)
+        
+        
+        contentView.addConstraint(NSLayoutConstraint(item: txtFirstImg, attribute: .centerY, relatedBy: .equal, toItem: contentView, attribute: .centerY, multiplier: 1, constant: 0))
+        
+        keepView(txtFirstImg, onPage: 1)
+        keepView(sprView, onPages: [0,1,2,3])
     }
-    */
+    
+    func sprAnimation() {
+        sprView.animation = "fadeIn"
+        sprView.duration = 9
+        sprView.animate()
+        
+        sprTxtLbl.animation = "zoomOut"
+        sprTxtLbl.duration = 8
+        sprTxtLbl.animate()
+    }
 
+   
 }
