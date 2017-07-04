b31396ec6403e6354b597c27ac8a0dbcba1f75a2
polin911 <polin911@mail.ru>
2017-07-04 14:13:32 +0300

    which Quize btn

--- RybaSushiQuize/WhichQuizeVC.swift
+++ RybaSushiQuize/WhichQuizeVC.swift
@@ -13,21 +13,19 @@ class WhichQuizeVC: UIViewController{
     var player = Player()
     
     @IBOutlet var nameLblTxt: UILabel!
-    @IBOutlet var tableView: UITableView!
+
     
      var whichQuizeChoose = ["Игра на роллы", "Игра на сашими", "Игра на устрицы с шампанским" ]
      var nameText = String()
      var whichQuize : WhichQuizeModel!
     
-    
+    @IBOutlet var btnR: UIButton!
+    @IBOutlet var btnS: UIButton!
+    @IBOutlet var btnU: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        
+
         nameLblTxt.text = "Привет \(player.playerName) ты готов перекусить ??"
        
         checking()
@@ -38,44 +36,25 @@ class WhichQuizeVC: UIViewController{
         let instructVC = segue.destination as? InstructionVC
        
         instructVC?.player = self.player
-        //whichQuize.howManyQuestions(nameOfGame: typeOf)
 }
     
     func checking() {
         print("WhichQuize@@@@@@@@@@@@@@@@@@@@@whichGame:\(player.wchiGame) @@@@@@@@@@@@@@name:\(player.playerName)@@@@@@@@@@@@@@@@@@@@\(player.playerScore) @@@@@@@@@@@@@name of Game: \(player.nameGame)")
     }
-}
-//MARK: TableView
 
-extension WhichQuizeVC: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let nameQuize = player.wchiGame[indexPath.row]
-        self.player.nameGame = nameQuize
-        performSegue(withIdentifier: "showInstructions", sender: nameQuize)
-       
-      
+    
+    @IBAction func btnPressed(_ sender: UIButton) {
+        if sender == btnR {
+            self.player.nameGame = "Игра на роллы"
+        }
+        if sender == btnS {
+            self.player.nameGame = "Игра на сашими"
+        }
+        if sender == btnU {
+            self.player.nameGame = "Игра на устрицы с шампанским"
+        }
+        performSegue(withIdentifier: "showInstructions", sender: self)
     }
+    
     
 }
-
-
-extension WhichQuizeVC: UITableViewDataSource {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return player.wchiGame.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "whichQuize") as! WhichQuizeCell
-        cell.quizeLabel.text = player.wchiGame[indexPath.row]
-        
-        return cell
-    }
-}
-
-
-
